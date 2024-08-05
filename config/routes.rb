b97f3adcd08d82edefe291e028f0ac8f17031787
FujiwaraChoki <fuji@fuji.codes>
2024-08-05 17:15:47 +0200

    feat: implement authentication (sign up only for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "main#index"
   get "about-us", to: "about#index", as: :about
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
