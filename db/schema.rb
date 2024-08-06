4559e78004f47de160c114e98d52777b2f538e07
FujiwaraChoki <fuji@fuji.codes>
2024-08-06 09:12:52 +0200

    feat: Add editPassword route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_135924) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_061651) do
+  create_table "currents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
